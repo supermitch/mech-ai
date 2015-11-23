f8ac907837e198ddac3d4ce9c5f72243c89b5ca1
Mitch LeBlanc <supermitch@gmail.com>
2015-11-22 17:33:53 -0800

    Enable environment variables for settings

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
-host = 'http://mech-ai.appspot.com'
+import os
+
+host_envs = {
+    'prod': 'http://mech-ai.appspot.com',
+    'dev': 'http://127.0.0.1:8080',
+}
+
+environment = os.getenv('ENV', 'dev')
+host = host_env.get('environment')
+
+username = os.getenv('USER')
+access_token = os.getenv('TOKEN')
 
 try:
-    from local_config import *  # Override with config-local if exists
+    from local_config import *  # Override with local settings if exists
 except ImportError:
     pass
 
