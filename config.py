cd10b7902d45f015d9abf8cc3bff765bfcca2984
Mitch LeBlanc <supermitch@gmail.com>
2015-12-06 22:08:30 -0800

    Fix bugs in config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-host_envs = {
+host_env = {
     'prod': 'http://mech-ai.appspot.com',
     'dev': 'http://127.0.0.1:8080',
 }
 
 environment = os.getenv('ENV', 'dev')
-host = host_env.get('environment')
+host = host_env.get(environment)
 
 username = os.getenv('USER')
 access_token = os.getenv('TOKEN')
