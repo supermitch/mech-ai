177bd7546faea56750a182c46a8fd6a892ff5d6a
Mitch LeBlanc <supermitch@gmail.com>
2015-12-21 20:33:16 -0800

    Update State turns, those aren't game attributes

--- game.py
+++ game.py
@@ -47,7 +47,8 @@ class Game(object):
     def update(self, username, move):
         """ Execute a round. """
         #TODO: Definitely somethign with the Queue! Not sure what at the moment...
-        self.current_turn += 1
-        if self.current_turn == self.max_turns:
+        self.queue.increment_move()
+        self.state.current_turn += 1
+        if self.state.current_turn == self.state.max_turns:
             self.status = GAME_STATUS.complete
 
