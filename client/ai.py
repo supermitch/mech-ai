12d927a8fa7d5cf45276fe8e1049cc6cd355cc03
Mitch LeBlanc <supermitch@gmail.com>
2016-01-25 20:43:08 -0800

    Add shoot to random AI

--- client/ai.py
+++ client/ai.py
@@ -1,19 +1,24 @@
 import random
 
-
-# define possible actions
+# Move definitions
 go_north = 'go north'
 go_south = 'go south'
 go_east = 'go east'
 go_west = 'go west'
-
 rotate_cw = 'rotate cw'
 rotate_ccw = 'rotate ccw'
-
 wait = 'wait'
-
+shoot = 'shoot'
 
 def make_move(state):
     """ Given a game state, decide on a move. """
-    print('AI making move for state: {}'.format(state))
-    return random.choice([go_north, go_south, go_east, go_west, rotate_cw, rotate_ccw])
+    # TODO: Implement AI!
+    return random.choice([
+        go_north,
+        go_south,
+        go_east,
+        go_west,
+        rotate_cw,
+        rotate_ccw,
+        shoot,
+    ])
