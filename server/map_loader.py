b1e8eae9e2661c8d8d3b000433da7c7548b7af5b
Mitch LeBlanc <supermitch@gmail.com>
2016-03-09 21:12:22 -0800

    Let's make maps as Python files

--- server/map_loader.py
+++ server/map_loader.py
@@ -2,25 +2,7 @@ import glob
 import logging
 import os
 
-def get_map_dir():
-    """ Helpfully return the path to the map folder. """
-    root = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(root, 'maps')
+import maps
 
-def read_map_file(map_name):
-    """ Load map data from disk. """
-    map_path = os.path.join(get_map_dir(), map_name + '.txt')
-
-    if not os.path.isfile(map_path):
-        logging.error('Map file [{}] does not exist'.format(map_path))
-    else:
-        try:
-            with open(map_path, 'r') as f:
-                return f.read()
-        except IOError:
-            logging.error('IOError exception reading map file [{}]'.format(map_path))
-
-def list_maps():
-    """ Return a list of maps in the map folder. """
-    return glob.glob(os.path.join(get_map_dir(), '*.txt'))
-
+logging.info(maps.default.map)
+logging.info(maps.hallway.map)
