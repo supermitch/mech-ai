aab7e134bb869a2d20edcc99dcc882e301d32e9a
Mitch LeBlanc <supermitch@gmail.com>
2016-03-18 22:48:36 -0700

    Extract map, split on newline, and iterate over chars

--- server/js/viewer.js
+++ server/js/viewer.js
@@ -4,8 +4,8 @@ function draw() {
     if (!canvas.getContext) {
         console.log('Could not load canvas context');
     } else {
+        draw_ui(canvas);
         draw_controls(canvas);
-        render_tiles(canvas);
 
         $.when($.getJSON('http://127.0.0.1:8080/api/v1/games/5066549580791808', function () {
             console.log('Initiated');
@@ -16,17 +16,18 @@ function draw() {
             console.log('Data:', data);
             var transactions = extract_transactions(data);
             console.log('Transactions:', transactions);
+            render_map(canvas, transactions[0].state.map);
         });
     }
+}
+
+function draw_ui(canvas) {
+    // stuff
 }
 
 function draw_controls(canvas) {
     // Render play control images.
     var ctx = canvas.getContext('2d');
-
-    var img = new Image();
-    img.onload = function(){ ctx.drawImage(img,0,0); };
-    img.src = '/images/madcat.gif';
 
     var icon_play = new Image();
     icon_play.onload = function(){ ctx.drawImage(icon_play, 0, canvas.height - 28); };
@@ -37,14 +38,34 @@ function draw_controls(canvas) {
     icon_pause.src = '/images/icon_pause.png';
 }
 
-function render_tiles(canvas) {
+function render_map(canvas, map) {
+    /// Render the map tiles
     var ctx = canvas.getContext('2d');
+    var w = 10, h = 10;  // tile width and height
+    var x = 0, y = 0;  // tile position
 
-    ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect (200, 100, 55, 50);
+    var red = 'rgb(200, 0, 0)';
+    var green = 'rgb(0, 200, 0)';
+    var blue = 'rgb(0, 0, 200)';
 
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect (300, 300, 55, 50);
+    console.log('Map:', JSON.stringify(map));
+    var rows = map.split('\n');
+    console.log('Rows:', rows);
+
+    rows.forEach(function (row) {
+        for (var x = 0; x < row.length; x++) {
+            var c = row.charAt(x);
+            console.log(x, c);
+        }
+    });
+
+    ctx.fillStyle = red;
+    x = 300; y = 200;
+    ctx.fillRect (x, y, w, h);
+
+    ctx.fillStyle = blue;
+    x = 100; y = 50;
+    ctx.fillRect (x, y, w, h);
 }
 
 function extract_transactions(data) {
